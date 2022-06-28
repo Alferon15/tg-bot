7593e8be2cdd97e75e2807106bb6b2f5ffaa34c8
alferon15 <alferon15@gmail.com>
2022-06-28 14:17:06 +1000

    python3.9

--- tg_bot/urls.py
+++ tg_bot/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from tg_bot.views import webhook_process
+from tg_bot.views import WebhookProcess
 
 urlpatterns = [
-    path('', webhook_process)
+    path('', WebhookProcess.as_view())
 ]
--- tg_bot/views.py
+++ tg_bot/views.py
@@ -1,17 +1,18 @@
 import logging
 
-from django.shortcuts import render, HttpResponse
+from django.views import View
+from django.http import JsonResponse
 from tg_bot.utils.bot import bot
 from django.views.decorators.csrf import csrf_exempt
 
 
-
 # Create your views here.
-@csrf_exempt
-async def webhook_process(request):
-    if request.method == "POST":
+class WebhookProcess(View):
+    def get(self, request, *args, **kwargs):  # for debug
+        return JsonResponse({"ok": "Get request received! But nothing done"})
+    @csrf_exempt
+    def post(self, request, *args, **kwargs):
         logging.warning('webhook_process!!!')
         print("Data received from Webhook is: ", request.body)
-        await bot.send_message(100204219, 'webhook_process')
-    else:
-        return HttpResponse('GET')
+        bot.send_message(100204219, 'webhook_process')
+        return JsonResponse({"ok": "POST request processed"})
