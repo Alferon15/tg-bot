d4cfee8bf89926b771915d4013e88906f4f976ab
alferon15 <alferon15@gmail.com>
2022-06-28 11:03:44 +1000

    webhook

--- tg_bot/views.py
+++ tg_bot/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from tg_bot.utils.bot import bot
 
 
 # Create your views here.
-def webhook_process(request):
-    bot.send_message(100204219, 'webhook_process')
-    return render(request)
+async def webhook_process(request):
+    if request.method == "POST":
+        bot.send_message(100204219, 'webhook_process')
+        print("Data received from Webhook is: ", request.body)
+        return HttpResponse('POST')
+    else:
+        return HttpResponse('GET')
