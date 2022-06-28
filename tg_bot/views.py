bd9f4231485e265171e57c61d34caa56c5f49380
alferon15 <alferon15@gmail.com>
2022-06-28 12:30:55 +1000

    csrf

--- tg_bot/views.py
+++ tg_bot/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse
 from tg_bot.utils.bot import bot
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 # Create your views here.
+@csrf_exempt
 async def webhook_process(request):
     if request.method == "POST":
         bot.send_message(100204219, 'webhook_process')
