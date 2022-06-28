21bc2242bee6e1475a2628eb491ecffcef58da7c
alferon15 <alferon15@gmail.com>
2022-06-28 11:47:02 +1000

    python3.9

--- tg_bot/utils/bot_config.py
+++ tg_bot/utils/bot_config.py
@@ -1,5 +1,5 @@
 API_TOKEN = '944887090:AAH8Udr9tTeILxWM33xGPkXeJcLmEpY_eio'
-SECRET = 'ihwkjbef8iou452903projif'
+SECRET = 'iwyfbsdknvl;asdfkm9p7348-0[pwrk'
 WEBHOOK_URL = 'https://alferon15.pythonanywhere.com/' + SECRET
 
 admin_id = 100204219
