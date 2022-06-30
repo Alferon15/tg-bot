67fc2381dfed0b4dc2c3f465bd0150c521959cd1
alferon15 <alferon15@gmail.com>
2022-07-01 09:41:52 +1000

    decode

--- tg_bot/utils/bot.py
+++ tg_bot/utils/bot.py
@@ -1,12 +1,9 @@
 import logging
 import telebot
-import json
 
+from telebot import types
 from tg_bot.utils.bot_config import API_TOKEN, admin_id
 
-
-WEBAPP_HOST = 'http://alferon15.pythonanywhere.com' 
-WEBAPP_PORT = 80
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
@@ -15,5 +12,6 @@ bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
 
 
 def process_webhook(body):
-    json_obj = json.loads(body)
-    bot.send_message(admin_id, json_obj)
+    json_obj = body.decode('UTF-8')
+    update = types.Update.de_json(json_obj)
+    bot.send_message(admin_id, update)
