2a59b524dcf536168258cf4c481c0689db2e8308
alferon15 <alferon15@gmail.com>
2022-07-01 10:25:07 +1000

    decode

--- tg_bot/utils/bot.py
+++ tg_bot/utils/bot.py
@@ -14,9 +14,5 @@ bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
 def process_webhook(body):
     json_obj = body.decode('UTF-8')
     update = types.Update.de_json(json_obj)
-    for u in update:
-        bot.send_message(admin_id, u.update_id)
-        bot.send_message(admin_id, u.message.entities)
-        bot.send_message(admin_id, u.message.from_user)
-        bot.send_message(admin_id, u.message)
-        bot.send_message(admin_id, u.message.date)
+    bot.send_message(admin_id, update)
+    bot.send_message(admin_id, update.message)
