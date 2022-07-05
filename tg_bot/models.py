de62068ed84bd26eeff6d4ae533f5916dec29e3f
alferon15 <alferon15@gmail.com>
2022-07-05 13:42:49 +1000

    models

--- tg_bot/models.py
+++ tg_bot/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class TGUser(models.Model):
+    class Meta:
+        ordering = ["user_name"]
+    
+    tg_id = models.IntegerField()
+    ally = models.BooleanField()
+    user_name = models.CharField(max_length=50, help_text="Enter user name")
+    
+    
+    
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return self.field_name
+    
+    
+class Printers(models.Model):
+    class Meta:
+        pass
+    
+    printer_name = models.CharField(max_lenght=100, help_text="")
+
+
+class Cartridges(models.Model):
+    class Meta:
+        pass
+    
+    cartridge_name = models.CharField(max_lenght=100, help_text="")
+    printers = models.ManyToManyField(Printers)
