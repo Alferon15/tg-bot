d8312c7043f0f8447302542f2c81d3ec151abf9e
alferon15 <alferon15@gmail.com>
2022-07-06 07:58:32 +1000

    MySQL

--- aaa_site/settings.py
+++ aaa_site/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
         'USER': 'alferon15',
         'PASSWORD': 'N89fdZ15',
         'HOST': 'alferon15.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
