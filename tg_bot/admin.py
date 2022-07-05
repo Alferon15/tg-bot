167a3be1d3bc03c15d93269c49dd311ce61ba29a
alferon15 <alferon15@gmail.com>
2022-07-06 09:41:58 +1000

    AdminRegister

--- tg_bot/admin.py
+++ tg_bot/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from tg_bot.models import TGUser, Printers, Cartridges
 
 # Register your models here.
+@admin.register(TGUser)
+class TGUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Printers)
+class PrintersAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cartridges)
+class CartridgesAdmin(admin.ModelAdmin):
+    pass
