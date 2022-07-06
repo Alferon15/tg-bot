b61b473618b59b36caaa0df6b2d3c23159743415
alferon15 <alferon15@gmail.com>
2022-07-06 12:50:24 +1000

    html

--- base_app/urls.py
+++ base_app/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from base_app.views import WebhookProcess
+from base_app.views import HomeView
 
 urlpatterns = [
-    path('', WebhookProcess.as_view())
+    path('', HomeView.as_view())
 ]
