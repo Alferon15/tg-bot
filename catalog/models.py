451a9f61d4eef523b16d3ed8c73b040647eb94bd
alferon15 <alferon15@gmail.com>
2022-07-11 15:01:25 +1000

    goods

--- catalog/models.py
+++ catalog/models.py
@@ -3,9 +3,9 @@ from statistics import mode
 from django.db import models
 
 class Good(models.Model):
-    name = models.TextField(verbose_name='Наменование товара')
-    caption = models.TextField(verbose_name='Описание товара', default='')
-    cost = models.IntegerField(verbose_name='Цена товара', default=0)
+    name = models.TextField(verbose_name='Наименование товара')
+    caption = models.TextField(verbose_name='Описание товара', blank=True, null=True, default='')
+    cost = models.IntegerField(verbose_name='Цена товара', blank=True, null=True, default=0)
     picture = models.ImageField(verbose_name='Изображение товара', upload_to='uploads/good_images/', blank=True, null=True)
 
     def __str__(self):
