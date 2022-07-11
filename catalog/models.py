f16b9f84134140a242fc4e99d71fdb41bea84783
alferon15 <alferon15@gmail.com>
2022-07-11 12:20:38 +1000

    goods

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Good(models.Model):
     name = models.TextField(verbose_name='Наменование товара')
-    caption = models.TextField(verbose_name='Описание товара')
+    caption = models.TextField(verbose_name='Описание товара', default='')
 
     def __str__(self):
         return self.name
