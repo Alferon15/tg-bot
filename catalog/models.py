05827032197e6188c6fab5e08e8a462d1a7be328
alferon15 <alferon15@gmail.com>
2022-07-11 13:59:32 +1000

    goods

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,12 @@
+from distutils.command.upload import upload
+from statistics import mode
 from django.db import models
 
 class Good(models.Model):
     name = models.TextField(verbose_name='Наменование товара')
     caption = models.TextField(verbose_name='Описание товара', default='')
+    cost = models.IntegerField(verbose_name='Цена товара', default=0)
+    picture = models.ImageField(verbose_name='Изображение товара', upload_to='uploads/good_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
