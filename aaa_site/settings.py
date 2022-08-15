225f218c9045fb2b9b3691668874cb52b3ee9a67
alferon15 <alferon15@gmail.com>
2022-08-15 14:55:46 +1000

    goods

--- aaa_site/settings.py
+++ aaa_site/settings.py
@@ -135,9 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/uploads/'
