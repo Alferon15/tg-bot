05a5a8c2101873003c67f20d87be9bbc9badeba7
alferon15 <alferon15@gmail.com>
2022-08-16 08:55:56 +1000

    goods

--- catalog/models.py
+++ catalog/models.py
@@ -7,7 +7,7 @@ class Good(models.Model):
     name = models.TextField(verbose_name='Наименование товара')
     caption = models.TextField(verbose_name='Описание товара', blank=True, null=True, default='')
     cost = models.IntegerField(verbose_name='Цена товара', blank=True, null=True)
-    picture = models.ImageField(verbose_name='Изображение товара', upload_to='uploads/good_images/', blank=True, null=True)
+    picture = models.ImageField(verbose_name='Изображение товара', upload_to='good_images/', blank=True, null=True)
 
 
     def get_absolute_url(self):
