0465ed921dc573c17eedc6af5b0383a217852bea
alferon15 <alferon15@gmail.com>
2022-08-17 09:06:52 +1000

    goods

--- catalog/models.py
+++ catalog/models.py
@@ -7,14 +7,11 @@ class Good(models.Model):
     caption = models.TextField(verbose_name='Описание товара', blank=True, null=True, default='')
     cost = models.IntegerField(verbose_name='Цена товара', blank=True, null=True)
 
-
     def get_absolute_url(self):
         return reverse('catalog:detail', kwargs={'pk': self.pk})
     
-
     def __str__(self):
         return self.name
-
 
     def image(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.picture.url))
@@ -24,3 +21,6 @@ class GoodImage(models.Model):
     good = models.ForeignKey(Good, related_name='images', on_delete=models.CASCADE)
     number = models.IntegerField(verbose_name='Номер изображения', blank=False, null=False)
     image = models.ImageField(verbose_name='Изображение товара', upload_to='good_images/', blank=True, null=True)
+
+    def good(self):
+        return self.good.name
