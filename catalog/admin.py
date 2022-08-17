5116ed054a4058bb3a93b55fc449e30ce9393e6d
alferon15 <alferon15@gmail.com>
2022-08-18 08:26:25 +1000

    goods

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,20 +1,25 @@
+from pyexpat import model
 from django.contrib import admin
 from catalog.models import Good, GoodImage
 
 # Register your models here.
 
 
-class GoodImageInline(admin.StackedInline):
+class GoodImageInline(admin.TabularInline):
     model = GoodImage
-
-
+    extra = 0 
+    
 @admin.register(Good)
 class GoodAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'cost', 'images']
+    list_display = ['name', 'image_picture', 'cost']
     list_display_links = ['name']
     inlines = [ GoodImageInline ]
 
 
 @admin.register(GoodImage)
 class GoodImageAdmin(admin.ModelAdmin):
-    list_display = ['good', 'number', 'image_pucture']
+    class Meta:
+        model = GoodImage
+    
+    inlines = [GoodImageInline]
+    list_display = ['number', 'image_pucture']
--- catalog/models.py
+++ catalog/models.py
@@ -6,21 +6,21 @@ class Good(models.Model):
     name = models.TextField(verbose_name='Наименование товара')
     caption = models.TextField(verbose_name='Описание товара', blank=True, null=True, default='')
     cost = models.IntegerField(verbose_name='Цена товара', blank=True, null=True)
+    image_main = models.ImageField(verbose_name='Изображение товара', upload_to='good_images/', blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+    
     def get_absolute_url(self):
         return reverse('catalog:detail', kwargs={'pk': self.pk})
     
-    def __str__(self):
-        return self.name
+    def image_pucture(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image_main.url))
 
 
 class GoodImage(models.Model):
-    good = models.ForeignKey(Good, related_name='images', on_delete=models.CASCADE)
-    number = models.IntegerField(verbose_name='Номер изображения', blank=False, null=False)
+    good = models.ForeignKey(Good, related_name='add_images', on_delete=models.CASCADE)
     image = models.ImageField(verbose_name='Изображение товара', upload_to='good_images/', blank=True, null=True)
 
     def image_pucture(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
-
-    def good(self):
-        return self.good.__name__
