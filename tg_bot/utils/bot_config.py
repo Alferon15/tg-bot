145087f742978bb7c628c566d9ffb116c5fa4016
Alferon15 <Alferon15@gmail.com>
2025-10-05 01:10:32 +1000

    Delete tg_bot/utils/bot_config.py

--- tg_bot/utils/bot_config.py
+++
@@ -1,5 +0,0 @@
-API_TOKEN = '944887090:AAH8Udr9tTeILxWM33xGPkXeJcLmEpY_eio'
-SECRET = 'iwyfbsdknvl;asdfkm9p7348-0[pwrk'
-WEBHOOK_URL = 'https://alferon15.pythonanywhere.com/' + SECRET
-
-admin_id = 100204219
